7eb906b7b4c48da3388f2017043876ab58461511
Ezequiel Birman <ebirman77@gmail.com>
2026-02-04 01:55:11 +0000

    Improve branch detection

--- pullAllRepos.sh
+++ pullAllRepos.sh
@@ -10,15 +10,12 @@ do
     then
 	echo -e "Updating \e[7m -----$repository----- \e[0m..."
 	pushd $repository
-        upstreamRef=($(git for-each-ref --format="%(refname:short)" refs/remotes/upstream/ma{in,ster}))
-        case ${#upstreamRef[@]} in
-            0)
-                git pull ;;
-            1)
-                git fetch upstream && git rebase ${upstreamRef[0]} ;;
-            *)
-                echo "There are a main and a master branch in upstream?!; most likely something is broken"
-        esac
+        upstreamRef=$(git ls-remote --symref upstream HEAD 2>/dev/null | awk '/^ref:/ {sub("refs/heads/", "", $2); print "upstream/" $2}')
+        if [[ -n "$upstreamRef" ]]; then
+            git fetch upstream && git rebase "$upstreamRef"
+        else
+            git pull
+        fi
 	popd
     else
 	echo "Repository $repository not cloned"
